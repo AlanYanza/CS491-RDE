643709065db146684270fba4ac059373ee95aed4
taylork2 <tkt7@njit.edu>
2018-04-02 01:09:23 -0400

    Sending emails via coldfusion test file works

--- message/src/app/message/message.service.ts
+++ message/src/app/message/message.service.ts
@@ -5,16 +5,16 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 @Injectable()
 export class MessageService {
 
-	sendMessageAPIUrl = "http://localhost:8500/CS491-RDE/rest/message.cfm";
+	sendMessageAPIUrl = "http://localhost:8500/rest/restTest/MessageSystem/sendEmail";
 
   constructor(private http: HttpClient) { }
 
   sendMessages(msg) {
   	const params = new HttpParams()
-  		.set("subject", msg.SUBJECT)
-  		.set("recipient", msg.RECEIPIENT)
-  		.set("message", msg.MESSAGE);  		;
-  	return this.http.post(this.sendMessageAPIUrl, null, {
+  		.set("subject", msg.subject)
+  		.set("recipient", msg.recipient)
+  		.set("message", msg.message);  		;
+  	return this.http.put(this.sendMessageAPIUrl, null, {
   		params: params
   	});
   }
