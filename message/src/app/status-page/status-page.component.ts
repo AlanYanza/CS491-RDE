5e585e8915207d0069ad8bd0365b932ffa3a632f
taylork2 <tkt7@njit.edu>
2018-04-18 23:48:57 -0400

    Create status page for message sending status

---
+++ message/src/app/status-page/status-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-status-page',
+  templateUrl: './status-page.component.html',
+  styleUrls: ['./status-page.component.css']
+})
+export class StatusPageComponent implements OnInit {
+
+	title='Welcome';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
