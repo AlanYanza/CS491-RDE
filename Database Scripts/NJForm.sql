232999e90d7cf0a4da6c5ed478cbf978965090f5
AlanYanza <Alan.Yanza@gmail.com>
2018-04-28 13:36:59 -0400

    added updated DatabaseScripts

---
+++ Database Scripts/NJForm.sql
@@ -0,0 +1,266 @@
+/*********************************************************************/
+/*Add the NJ Form to the Database
+Tables:NJSection1
+       NJSection2
+	   NJSecion3A
+	   NJSection3B*/
+/********************************************************************/
+/*replace first line with USE[DatabaseName]*/
+USE [RDESystems];
+--Check if Base Tables created using RDEDatabase.sql if not Do not attempt to create NJForm Tables
+IF (NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME=N'Forms') AND
+	NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME=N'UserFormData') AND
+	NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME=N'ApplicationLinks') )
+	PRINT 'ERROR: Mising Required Base Tables, Please Install Base Tables using RDEDatabase.sql and Retry';
+ELSE
+BEGIN
+	PRINT 'Adding NJ Form Tables to Database....';
+	--Insert NJ Form into Forms Table
+	IF (NOT EXISTS(SELECT state FROM Forms WHERE state='NJ' AND AppDescription='ADDP/HICP'))
+	BEGIN
+		INSERT INTO Forms(state,AppDescription) VALUES('NJ','ADDP/HICP');
+	END
+
+	--Retrieve FormTypeID of Form
+	DECLARE @NJFORMID int;
+	SET @NJFORMID = (SELECT formTypeID FROM FORMS WHERE state='NJ' AND AppDescription='ADDP/HICP' );
+
+	--Insert form into ApplicationLinks Table
+	IF (NOT EXISTS(SELECT * FROM ApplicationLinks WHERE state='NJ' AND formTypeID=@NJFORMID))
+		INSERT INTO ApplicationLinks VALUES('NJ',1,'/CS491-RDE/application/dhas_page1.cfm');
+
+	--Insert Form Tables into FormTables
+	IF (NOT EXISTS(SELECT * FROM FormTables WHERE formTypeID=@NJFORMID AND tableName='NJSection1'))
+		INSERT INTO FormTables(formTypeID,tableName) VALUES(@NJFORMID,'NJSection1');
+	IF (NOT EXISTS(SELECT * FROM FormTables WHERE formTypeID=@NJFORMID AND tableName='NJSection2'))
+		INSERT INTO FormTables(formTypeID,tableName) VALUES(@NJFORMID,'NJSection2');
+	IF (NOT EXISTS(SELECT * FROM FormTables WHERE formTypeID=@NJFORMID AND tableName='NJSection3A'))
+		INSERT INTO FormTables(formTypeID,tableName) VALUES(@NJFORMID,'NJSection3A');
+	IF (NOT EXISTS(SELECT * FROM FormTables WHERE formTypeID=@NJFORMID AND tableName='NJSection3B'))
+		INSERT INTO FormTables(formTypeID,tableName) VALUES(@NJFORMID,'NJSection3B');
+	IF (NOT EXISTS(SELECT * FROM FormTables WHERE formTypeID=@NJFORMID AND tableName='NJSection4'))
+		INSERT INTO FormTables(formTypeID,tableName) VALUES(@NJFORMID,'NJSection4');
+
+----------------------------------------------------------------------------------------------------
+	--Create NJSection1 Table
+	IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+				  WHERE TABLE_NAME=N'NJSection1')
+	BEGIN
+		CREATE TABLE [NJSection1] (
+			dataID int NOT NULL FOREIGN KEY REFERENCES [UserFormData](dataID),
+			FName varchar(100) NOT NULL DEFAULT(''),
+			MName varchar(100) NOT NULL DEFAULT(''),
+			LName varchar(100) NOT NULL DEFAULT(''),
+			DateBirth date DEFAULT NULL,
+			gender varchar(3) CHECK(gender IN('M','F','TMF','TFM','X')) DEFAULT('X'),
+			genderBirth char(1) CHECK(genderBirth IN('M','F','X')) DEFAULT('X'),
+			SSNum varchar(15) NOT NULL DEFAULT(''),
+			street varchar(50) NOT NULL DEFAULT(''),
+			aptNum varchar(9) NOT NULL DEFAULT(''),
+			city varchar(50) NOT NULL DEFAULT(''),
+			state varchar(2) NOT NULL DEFAULT(''),
+			zip varchar(12) NOT NULL DEFAULT(''),
+			county varchar(50) NOT NULL DEFAULT(''),
+			HPhone varchar(16) NOT NULL DEFAULT(''),
+			WPhone varchar(16) NOT NULL DEFAULT(''),
+			CPhone varchar(16) NOT NULL DEFAULT(''),
+			VHomeMsgPerm char(1) CHECK(VHomeMsgPerm IN('Y','N')) DEFAULT('N'),
+			VCellMsgPerm char(1) CHECK(VCellMsgPerm IN('Y','N')) DEFAULT('N'),
+			VWorkMsgPerm char(1) CHECK(VWorkMsgPerm IN('Y','N')) DEFAULT('N'),
+			AltComPerm char(1) CHECK(AltComPerm IN('Y','N','X')) DEFAULT('X'),
+			AddrRes char(1) CHECK(AddrRes IN('Y','N','X')) DEFAULT('X'),
+			MStreet varchar(50) NOT NULL DEFAULT(''),
+			MCity varchar(50) NOT NULL DEFAULT(''),
+			MState varchar(2) NOT NULL DEFAULT(''),
+			MZip varchar(12) NOT NULL DEFAULT(''),
+			MCounty varchar(50) NOT NULL DEFAULT(''),
+			MaptNum varchar(9) NOT NULL DEFAULT(''),
+			USCitizen char(1) CHECK(USCitizen IN('Y','N','X')) DEFAULT('X'),
+			veteran char(1) CHECK(veteran IN('Y','N','X')) DEFAULT('X'),
+			RelStatus varchar(2) CHECK(RelStatus IN('S','M','C','DP','D','W','SP','X')) DEFAULT('X'),
+			RWhite varchar(1) CHECK(RWhite IN('Y','N')) DEFAULT('N'),
+			RBlack varchar(1) CHECK(RBlack IN('Y','N')) DEFAULT('N'),
+			RAsian varchar(1) CHECK(RAsian IN('Y','N')) DEFAULT('N'),
+				AAsInd varchar(1) CHECK(AAsInd IN('Y','N')) DEFAULT('N'),
+				AChinese varchar(1) CHECK(AChinese IN('Y','N')) DEFAULT('N'),
+				AFilipino varchar(1) CHECK(AFilipino IN('Y','N')) DEFAULT('N'),
+				AJap varchar(1) CHECK(AJap IN('Y','N')) DEFAULT('N'),
+				AKorean varchar(1) CHECK(AKorean IN('Y','N')) DEFAULT('N'),
+				AViet varchar(1) CHECK(AViet IN('Y','N')) DEFAULT('N'),
+				AOther varchar(1) CHECK(AOther IN('Y','N')) DEFAULT('N'),
+			RAmInd varchar(1) CHECK(RAmInd IN('Y','N')) DEFAULT('N'),
+			RNatHa varchar(1) CHECK(RNatHa IN('Y','N')) DEFAULT('N'),
+				NHNatHaw varchar(1) CHECK(NHNatHaw IN('Y','N')) DEFAULT('N'),
+				NHGua varchar(1) CHECK(NHGua IN('Y','N')) DEFAULT('N'),
+				NHSam varchar(1) CHECK(NHSam IN('Y','N')) DEFAULT('N'),
+				NHOther varchar(1) CHECK(NHOther IN('Y','N')) DEFAULT('N'),
+			RUnk varchar(1) CHECK(RUnk IN('Y','N')) DEFAULT('N'),
+			EHisp varchar(1) CHECK(EHisp IN('Y','N','X')) DEFAULT('X'),
+				HispMex varchar(1) CHECK(HispMex IN('Y','N')) DEFAULT('N'),
+				HispPR varchar(1) CHECK(HispPR IN('Y','N')) DEFAULT('N'),
+				HispCub varchar(1) CHECK(HispCub IN('Y','N')) DEFAULT('N'),
+				HispOther varchar(1) CHECK(HispOther IN('Y','N')) DEFAULT('N'),
+			preg char(1) CHECK(preg IN('Y','N','X')) DEFAULT('X'),
+		);
+		PRINT 'Successfully created "NJSection1" Table';
+	END
+	ELSE
+		PRINT '"NJSection1" Table already exist';
+----------------------------------------------------------------------------------------------------
+	--Create NJSection2 Table
+	IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+				  WHERE TABLE_NAME=N'NJSection2')
+	BEGIN
+		CREATE TABLE [NJSection2] (
+			dataID int NOT NULL FOREIGN KEY REFERENCES [UserFormData](dataID),
+			EmplyStatus char(1) CHECK(EmplyStatus IN('F','P','N','X')) DEFAULT('X'),
+			unableWork char(1) CHECK(unableWork IN('Y','N','X')) DEFAULT('X'),
+			unable12LMonth char(1) CHECK(unable12LMonth IN('Y','N','X')) DEFAULT('X'),
+			unable12MMonth char(1) CHECK(unable12MMonth IN('Y','N','X')) DEFAULT('X'),
+			HPersonNum int DEFAULT(0),
+			salary decimal(14,2) DEFAULT(0.00),
+			disBen decimal(14,2) DEFAULT(0.00),
+			genAssist decimal(14,2) DEFAULT(0.00),
+			unemploy decimal(14,2) DEFAULT(0.00),
+			socialSecurity decimal(14,2) DEFAULT(0.00),
+			pension decimal(14,2) DEFAULT(0.00),
+			allimony decimal(14,2) DEFAULT(0.00),
+			OtherIncome decimal(14,2) DEFAULT(0.00),
+			totalHIncome decimal(14,2) DEFAULT(0.00),
+			taxFile char(1) CHECK(taxFile IN('Y','N')) DEFAULT('N'),
+			dependant char(1) CHECK(dependant IN('Y','N')) DEFAULT('N'),
+			TANF char(1) CHECK(TANF IN('A','R','X')) DEFAULT('X'),
+			SAIF char(1) CHECK(SAIF IN('A','R','X')) DEFAULT('X'),
+			SSI char(1) CHECK(SSI IN('A','R','X')) DEFAULT('X'),
+			GA char(1) CHECK(GA IN('A','R','X')) DEFAULT('X'),
+			SNAP char(1) CHECK(SNAP IN('A','R','X')) DEFAULT('X'),
+		);
+		PRINT 'Successfully created "NJSection2" Table';
+	END
+	ELSE
+	PRINT '"NJSection2" Table already exist';
+----------------------------------------------------------------------------------------------------
+	--Create NJSection3A Table
+	IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+				  WHERE TABLE_NAME=N'NJSection3A')
+	BEGIN
+		CREATE TABLE [NJSection3A] (
+			dataID int NOT NULL FOREIGN KEY REFERENCES [UserFormData](dataID),
+			insured char(1) CHECK(insured IN('Y','N','U','X')) DEFAULT('X'),
+			Medium char(1) CHECK(Medium IN('C','F','U','S','X')) DEFAULT('X') ,
+			EmpUnName varchar(100) NOT NULL DEFAULT(''),
+			EmpUnAddr varchar(200) NOT NULL DEFAULT(''),
+			EmpUnContact varchar(150) NOT NULL DEFAULT(''),
+			EmpUnPhone varchar(16) NOT NULL DEFAULT(''),
+			Medicaid char(1) CHECK(Medicaid IN('Y','N')) DEFAULT('N'),
+			MedicareAB char(1) CHECK(MedicareAB IN('Y','N')) DEFAULT('N'),
+			MedicareD char(1) CHECK(MedicareD IN('Y','N')) DEFAULT('N'),
+			PrivateIns char(1) CHECK(PrivateIns IN('Y','N')) DEFAULT('N'),
+			CHIP char(1) CHECK(CHIP IN('Y','N')) DEFAULT('N'),
+			COBRA char(1) CHECK(COBRA IN('Y','N')) DEFAULT('N'),
+			COBRAStart date DEFAULT NULL,
+			COBRAEnd date DEFAULT NULL,
+			NxtPremDue date DEFAULT NULL,
+			Other varchar(50) NOT NULL default(''),
+			PresCov char(1) CHECK(PresCov IN('Y','N','U','X')) DEFAULT('X'),
+			PresCovCap char(1) CHECK(PresCovCap IN('Y','N','U','X')) DEFAULT('X'),
+			PresCovCapAmt decimal(14,4) DEFAULT(0.00),
+			PresCovMail char(1) CHECK(PresCovMail IN('Y','N','P','X')) DEFAULT('X'),
+			AMedicaid char(1) CHECK(AMedicaid IN('Y','N','U','X')) DEFAULT('X'),
+			AMedicaidDate date DEFAULT NULL,
+			UMedicaidDate char(1) CHECK(UMedicaidDate IN('Y','N')) DEFAULT('N'),
+			RespMedicaid char(1) CHECK(RespMedicaid IN('Y','N','X')) DEFAULT('X'),
+			AMedicare char(1) CHECK(AMedicare IN('Y','N','U','X')) DEFAULT('X'),
+			AMedicareDate date DEFAULT NULL,
+			UMedicareDate char(1) CHECK(UMedicareDate IN('Y','N')) DEFAULT('N'),
+			RespMedicare char(1) CHECK(RespMedicare IN('Y','N','X')) DEFAULT('X'),
+			AMedicareD char(1) CHECK(AMedicareD IN('Y','N','U','X')) DEFAULT('X'),
+			ALIS char(1) CHECK(ALIS IN('Y','N','U','X')) DEFAULT('X'),
+		);
+		PRINT 'Successfully created "NJSection3A" Table';
+	END
+	ELSE
+		PRINT '"NJSection3A" Table already exist';
+----------------------------------------------------------------------------------------------------
+	--Create NJSection3B Table
+	IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+				  WHERE TABLE_NAME=N'NJSection3B')
+	BEGIN
+		CREATE TABLE [NJSection3B] (
+			dataID int NOT NULL FOREIGN KEY REFERENCES [UserFormData](dataID),
+			SSISSDIStatus varchar(5) CHECK(SSISSDIStatus IN('YSSI','YSSDI','N','U','X')) DEFAULT('X'),
+			ASSISSDIDate date DEFAULT NULL,
+			UASSISSDIDate char(1) CHECK(UASSISSDIDate IN('Y','N','X')) DEFAULT('X'),
+			RespASSISSI char(1) CHECK(RespASSISSI IN('Y','N','X')) DEFAULT('X'),
+			AMarket char(1) CHECK(AMarket IN('Y','N','U','X')) DEFAULT('X'),
+			AMarketDate date DEFAULT NULL,
+			UMarketDate char(1) CHECK(UMarketDate IN('Y','N')) DEFAULT('N'),
+			RespAMarket char(1) CHECK(RespAMarket IN('Y','N','X')) DEFAULT('X'),
+			CovMed char(1) CHECK(CovMed IN('Y','N')) DEFAULT('N'),
+			CovPres char(1) CHECK(CovPres IN('Y','N')) DEFAULT('N'),
+			CovOther char(1) CHECK(CovOther IN('Y','N')) DEFAULT('N'),
+			CovOtherTxt varchar(100) NOT NULL DEFAULT(''),
+			relation char(1) CHECK(relation IN('S','P','C','O','X')) DEFAULT('X'),
+			relOther varchar(50) NOT NULL DEFAULT(''),
+			InsName varchar(100) NOT NULL DEFAULT(''),
+			InsAddr varchar(200) NOT NULL DEFAULT(''),
+			InsAddrCounty varchar(50) NOT NULL DEFAULT(''),
+			InsSS varchar(15) NOT NULL DEFAULT(''),
+			InsPhone varchar(16) NOT NULL DEFAULT(''),
+			InsCarrier varchar(100) NOT NULL DEFAULT(''),
+			InsCarAddr varchar(200) NOT NULL DEFAULT(''),
+			InsCarPhone varchar(16) NOT NULL DEFAULT(''),
+			InsCarPolicyNum varchar(50) NOT NULL DEFAULT(''),
+			EmpUnName varchar(100) NOT NULL DEFAULT(''),
+			EmpUnAddr varchar(200) NOT NULL DEFAULT(''),
+			PresCar varchar(50) NOT NULL DEFAULT(''),
+			PresCarAddr varchar(200) NOT NULL DEFAULT(''),
+			PresCarPhone varchar(16) NOT NULL DEFAULT(''),
+			PresCarID varchar(50) NOT NULL DEFAULT(''),
+			PresCoPay decimal(14,2) DEFAULT(0.00),
+			PresDeduct decimal(14,2) DEFAULT(0.00),
+			EligVetDrugBen char(1) CHECK(EligVetDrugBen IN('Y','N','X')) DEFAULT('X'),
+			RecPresDrugBen char(1) CHECK(RecPresDrugBen IN('Y','N','X')) DEFAULT('X'),
+		);
+		PRINT 'Successfully created "NJSection3B" Table';
+	END
+	ELSE
+		PRINT '"NJSection3B" Table already exist';
+----------------------------------------------------------------------------------------------------
+		--Create NJSection4 Table
+		IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+					  WHERE TABLE_NAME=N'NJSection4')
+		BEGIN
+			CREATE TABLE [NJSection4] (
+				dataID int NOT NULL FOREIGN KEY REFERENCES [UserFormData](dataID),
+				CMName varchar(100) NOT NULL DEFAULT(''),
+				agency varchar(100) NOT NULL DEFAULT(''),
+				CMaddr varchar(200) NOT NULL DEFAULT(''),
+				CMWPhone varchar(16) NOT NULL DEFAULT(''),
+				CMFaxNum varchar(16) NOT NULL DEFAULT(''),
+				CMCPhone varchar(16) NOT NULL DEFAULT(''),
+				email varchar(100) NOT NULL DEFAULT(''),
+				perm char(1) CHECK (perm IN('Y','N','X')) DEFAULT('X'),
+				HIVAware char(1) CHECK (HIVAware IN('Y','N','X')) DEFAULT('X'),
+				CPName varchar(100) NOT NULL DEFAULT(''),
+				relation varchar(50) NOT NULL DEFAULT(''),
+				CPAddr varchar(200) NOT NULL DEFAULT(''),
+				CPHPhone varchar(16) NOT NULL DEFAULT(''),
+				CPWPhone varchar(16) NOT NULL DEFAULT(''),
+				CPCPhone varchar(16) NOT NULL DEFAULT(''),
+				PName varchar(100) NOT NULL DEFAULT(''),
+				PPhone varchar(16) NOT NULL DEFAULT(''),
+				--signature varbinary(max) DEFAULT (NULL),
+				signature varchar(max) DEFAULT (NULL),
+				signatureDate date DEFAULT GETDATE(),
+				--spouseSig varbinary(max) DEFAULT (NULL),
+				spouseSig varchar(max) DEFAULT (NULL),
+				spouseSigDate date DEFAULT GETDATE()
+			);
+			PRINT 'Successfully created "NJSection4" Table';
+		END
+		ELSE
+			PRINT '"NJSection4" Table already exist';
+----------------------------------------------------------------------------------------------------
+	PRINT 'Successfully Added NJ Form Tables to Database';
+END
---
+++ Database Scripts/RDEDatabase.sql
@@ -0,0 +1,173 @@
+/*********************************************************************/
+/*Creates Initial Base RDE Database (without any Forms)
+Note:There is a separate SQL script to add each Form
+     See documentation ERD diagrams for Database Design*/
+/********************************************************************/
+/*replace first line with USE[DatabaseName]*/
+USE [RDESystems];
+PRINT 'Creating Base Tables for RDE Form Application....';
+--Create User Table
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+              WHERE TABLE_NAME=N'User')
+BEGIN
+	CREATE TABLE [User] (
+		userID int PRIMARY KEY IDENTITY(11,1),
+		email varchar(150) NOT NULL,
+		FirstName varchar(150) NOT NULL,
+		LastName varchar(150) NOT NULL,
+		password binary(64) NOT NULL,
+		accessLevel varchar(50) NOT NULL
+		CHECK (accessLevel IN('user','admin'))
+	);
+	PRINT 'Successfully created "User" Table';
+END
+ELSE
+	PRINT '"User" Table already exist';
+-----------------------------------------------------------------------------------
+--Create Forms Table
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+			  WHERE TABLE_NAME=N'Forms')
+BEGIN
+	CREATE TABLE [Forms] (
+		formTypeID int IDENTITY(1,1) PRIMARY KEY,
+		state varchar(2) NOT NULL,
+		AppDescription varchar(500) NOT NULL Default('')
+	);
+	PRINT 'Successfully created "Forms" Table';
+END
+ELSE
+	PRINT '"Forms" Table already exist';
+-----------------------------------------------------------------------------------
+--Create UserApplication Table
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+			  WHERE TABLE_NAME=N'UserApplication')
+BEGIN
+	CREATE TABLE [UserApplication] (
+		userID int NOT NULL FOREIGN KEY REFERENCES [User](userID),
+		appID int IDENTITY(1,1) PRIMARY KEY,
+		formTypeID int NOT NULL FOREIGN KEY REFERENCES [Forms](formTypeID),
+		HICPApp char(1) NOT NULL CHECK (HICPApp IN('Y','N')),
+		dateEligDet date DEFAULT(NULL),
+		dateSubmited date DEFAULT(NULL),
+		status char(1) NOT NULL DEFAULT('R') CHECK (status IN('R','A','D','N','P'))
+	);
+	PRINT 'Successfully created "UserApplication" Table';
+END
+ELSE
+	PRINT '"UserApplication" Table already exist';
+---------------------------------------------------------------------------------
+--Create Message Table
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+			  WHERE TABLE_NAME=N'Message')
+BEGIN
+	CREATE TABLE [Message] (
+		msgID int IDENTITY(1,1) PRIMARY KEY,
+		senderID int NOT NULL,
+		recipientID int NOT NULL,
+		subject varchar(100) NOT NULL DEFAULT(''),
+		message varchar(max) NOT NULL DEFAULT(''),
+		dateSent datetime DEFAULT(NULL),
+		dateRecv datetime DEFAULT(NULL),
+		readStatus char(1) NOT NULL CHECK (readStatus IN('T','F'))
+	);
+	PRINT 'Successfully created "Message" Table';
+END
+ELSE
+	PRINT '"Message" Table already exist';
+---------------------------------------------------------------------------------
+--Create FormTables Table
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+			  WHERE TABLE_NAME=N'FormTables')
+BEGIN
+	CREATE TABLE [FormTables] (
+		formTypeID int NOT NULL FOREIGN KEY REFERENCES [Forms](formTypeID),
+		tableName varchar(100) NOT NULL
+	);
+	PRINT 'Successfully created "FormTables" Table';
+END
+ELSE
+	PRINT '"FormTables" Table already exist';
+---------------------------------------------------------------------------------
+--Create AppDocument Table
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+			  WHERE TABLE_NAME=N'AppDocument')
+BEGIN
+	CREATE TABLE [AppDocument] (
+		appID int NOT NULL FOREIGN KEY REFERENCES [UserApplication](appID),
+		document varchar(1000) NOT NULL DEFAULT(''),
+		isRequired char(1) NOT NULL CHECK (isRequired IN('Y','N')) DEFAULT('N'),
+		received char(1) NOT NULL CHECK (received IN('Y','N')) DEFAULT('N'),
+		dateReceived date
+	);
+	PRINT 'Successfully created "AppDocument" Table';
+END
+ELSE
+	PRINT '"AppDocument" Table already exist';
+---------------------------------------------------------------------------------
+--Create Inbox Table
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+			  WHERE TABLE_NAME=N'Inbox')
+BEGIN
+	CREATE TABLE [Inbox] (
+		userID int NOT NULL FOREIGN KEY REFERENCES [User](userID),
+		msgID int NOT NULL FOREIGN KEY REFERENCES [Message](msgID),
+	);
+	PRINT 'Successfully created "Inbox" Table';
+END
+ELSE
+	PRINT '"Inbox" Table already exist';
+---------------------------------------------------------------------------------
+--Create Sent Table
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+			  WHERE TABLE_NAME=N'Sent')
+BEGIN
+	CREATE TABLE [Sent] (
+		userID int NOT NULL FOREIGN KEY REFERENCES [User](userID),
+		msgID int NOT NULL FOREIGN KEY REFERENCES [Message](msgID)
+	);
+	PRINT 'Successfully created "Sent" Table';
+END
+ELSE
+	PRINT '"Sent" Table already exist';
+---------------------------------------------------------------------------------
+--Create Trash Table
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+			  WHERE TABLE_NAME=N'Trash')
+BEGIN
+	CREATE TABLE [Trash] (
+		userID int NOT NULL FOREIGN KEY REFERENCES [User](userID),
+		msgID int NOT NULL FOREIGN KEY REFERENCES [Message](msgID)
+	);
+	PRINT 'Successfully created "Trash" Table';
+END
+ELSE
+	PRINT '"Trash" Table already exist';
+---------------------------------------------------------------------------------
+--Create UserFormData Table
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+			  WHERE TABLE_NAME=N'UserFormData')
+BEGIN
+	CREATE TABLE [UserFormData] (
+		appID int NOT NULL FOREIGN KEY REFERENCES [UserApplication](appID),
+		dataID int IDENTITY(1,1) PRIMARY KEY
+	);
+	PRINT 'Successfully created [UserFormData] Table';
+END
+ELSE
+	PRINT '"UserFormData" Table already exist';
+---------------------------------------------------------------------------------
+--Create ApplicationLinks Table
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES
+			  WHERE TABLE_NAME=N'ApplicationLinks')
+BEGIN
+	CREATE TABLE [ApplicationLinks] (
+		state varchar(2) PRIMARY KEY,
+		formTypeID int NOT NULL FOREIGN KEY REFERENCES [Forms](formTypeID),
+		directLink varchar(200)
+	);
+	PRINT 'Successfully created [ApplicationLinks] Table';
+END
+ELSE
+	PRINT '"ApplicationLinks" Table already exist';
+---------------------------------------------------------------------------------
+PRINT 'Successfully created Base Tables for RDE Form Application';
